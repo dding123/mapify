c767caccab97cc6138aa5b99b0fc8b22a40bbfd7
Tyler Rodgers <31381200+tylrodg@users.noreply.github.com>
2018-02-07 10:38:24 -0600

    pulls user's top artists top tracks

--- base-project/js/main.js
+++ base-project/js/main.js
@@ -1,9 +1,9 @@
-(function() {
+(function () {
   'use strict';
 
-  var ViewModel = function() {
+  var ViewModel = function () {
     this.isLoggedIn = ko.observable(false);
-    this.login = function() {
+    this.login = function () {
       var self = this;
       this.loginErrorMessage(null);
       OAuthManager.obtainToken({
@@ -13,29 +13,29 @@
             automatically when obtaining an access token through
             the user login form
             */
-            'user-read-private',
-            'user-top-read'
-          ]
-        }).then(function(token) {
-          onTokenReceived(token);
-        }).catch(function(error) {
-          self.loginError(error);
-        });
+          'user-read-private',
+          'user-top-read'
+        ]
+      }).then(function (token) {
+        onTokenReceived(token);
+      }).catch(function (error) {
+        self.loginError(error);
+      });
     };
 
-    this.logout = function() {
+    this.logout = function () {
       localStorage.removeItem(accessTokenKey);
       this.isLoggedIn(false);
     };
 
-    this.loginError = function(errorCode) {
+    this.loginError = function (errorCode) {
       switch (errorCode) {
         case 'access_denied':
           this.loginErrorMessage('You need to grant access in order to use this website.');
           break;
         default:
           this.loginErrorMessage('There was an error trying to obtain authorization. Please, try it again later.');
-        }
+      }
     };
 
     this.loginErrorMessage = ko.observable(null);
@@ -43,13 +43,15 @@
     this.user = ko.observable(null);
 
     this.artists = ko.observableArray([]);
+    this.a = ko.observableArray([]);
   };
 
   var viewModel = new ViewModel();
   ko.applyBindings(viewModel);
 
+
   var spotifyApi = new SpotifyWebApi(),
-      accessTokenKey = 'sp-access-token';
+    accessTokenKey = 'sp-access-token';
 
   function onTokenReceived(token) {
     viewModel.isLoggedIn(true);
@@ -57,12 +59,16 @@
 
     // do something with the token
     spotifyApi.setAccessToken(token);
-    spotifyApi.getMe().then(function(data) {
+    spotifyApi.getMe().then(function (data) {
       viewModel.user(data);
 
-      spotifyApi.getMyTopArtists().then(function(artists) {
+      spotifyApi.getMyTopArtists({ limit: 5 }).then(function (artists) {
         console.log(artists);
         viewModel.artists(artists.items);
+        Promise.all(artists.items.map(a => spotifyApi.getArtistTopTracks(a.id, "US").then(function(a){
+          console.log(a);
+          viewModel.a(a.tracks);
+        })))
       });
     });
   }
